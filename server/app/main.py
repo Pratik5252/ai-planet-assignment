75187e95401e1a707b66e06576bd9d0edbdc0607
Pratik Yesane <pratiky2000@gmail.com>
2025-09-16 00:19:55 +0530

    fix: Correct CORS origin URL by removing trailing slash

--- server/app/main.py
+++ server/app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
-        "https://ai-planet-assignment-eta.vercel.app/",
+        "https://ai-planet-assignment-eta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
